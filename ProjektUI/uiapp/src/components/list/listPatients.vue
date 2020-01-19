9322b302054829e33279125451b188e02b14806d
Norbert <norbertskulski@wp.pl>
2020-01-19 19:33:44 +0100

    sad

--- ProjektUI/uiapp/src/components/list/listPatients.vue
+++ ProjektUI/uiapp/src/components/list/listPatients.vue
@@ -1,13 +1,14 @@
 <template>
     <div id="list">
         <!-- wyszukiwanie tu -->
+        <input id="Input" type="number" placeholder="Pesel" v-model="Wwyszukaj"><button id="Button" @click="wyszukaj">Wyszukaj</button>
         <div id="contentList">
             
             <div id="headers">
              <a id="first"> # </a> <a> Pesel </a> <a> Imie </a> <a> Nazwisko </a> <a id="last"> Opłaty </a>
             </div>
            
-            <div id="row" v-for="(item,index) in data" v-bind:key="item">
+            <div id="row" v-for="(item,index) in (!wyszukane[0]?data:wyszukane)" v-bind:key="item">
                 <a>{{index}}</a> <a>{{item.pesel}}</a> <a>{{item.name}}</a> <a>{{item.surrname}}</a> <a>{{item.costs}} zł</a> <a>{{item.nr_Budynku}}</a>
                  <img id="icons" @click="openModal(item.id)" src="../../icons/info-icon.svg"/> <img id="icons" @click="_delete(item.id)" src="../../icons/trash-icon.svg"/>
             </div>
@@ -21,6 +22,12 @@ import '../../icons/info-icon.svg'
 import { checkAccess } from '../../seciurity/sciurityUtils';
 import modifyPatientsModal from'../modals/Patients/modifyPatientsModal';
 export default {
+    data(){
+        return{
+            Wwyszukaj:undefined,
+            wyszukane:[]
+        }
+    },
     props:{
         name:{
             default:"lista"
@@ -47,6 +54,11 @@ export default {
             await this.$store.commit("deleteData",payload)
             await this.$store.commit("getData","http://localhost:8080/api/Buildings/getAll")
             await location.reload();
+        },
+        wyszukaj(){
+            window.console.log("wyszukaj:",this.Wwyszukaj);
+            this.wyszukane=this.data.filter(e=>e.pesel===this.Wwyszukaj);
+            window.console.log("wyszukane!!:",this.wyszukane);
         }
     }
 }
@@ -56,6 +68,15 @@ export default {
 
 *{
    background-image: none;
+}
+
+#Input{
+    margin-left: 1.7vw;
+}
+
+#Button{
+    width: auto;
+    height: auto;
 }
 
 #row a{
